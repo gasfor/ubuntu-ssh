58b5aab274aa78476baebaddbbe06d1c595da910
gasfor <48077766+gasfor@users.noreply.github.com>
2019-10-27 11:35:29 +0800

    fix user home path

--- start.sh
+++ start.sh
@@ -42,61 +42,6 @@ function __get_ssh_user ()
 	printf -- '%s' "${value}"
 }
 
-function __is_valid_ssh_user_home ()
-{
-	local -r home_directory="${1}"
-	local -r user_directory='^\/(?!\/|bin|dev|etc|lib|lib64|lost+found|media|proc|root|sbin|srv|sys|tmp|usr).+$'
-	local -r root_directory='^/root$'
-	local -r user="${2:-"$(
-		__get_ssh_user
-	)"}"
-
-	local safe_directory="${user_directory}"
-
-	if [[ -z ${home_directory} ]]
-	then
-		return 1
-	fi
-
-	if [[ ${user} == root ]]
-	then
-		safe_directory="${root_directory}"
-	fi
-
-	if grep -qoP "${safe_directory}" <<< "${home_directory}"
-	then
-		return 0
-	fi
-
-	return 1
-}
-
-function __get_ssh_user_home ()
-{
-	local -r default_value="${1:-/home/%u}"
-	local -r root_value="/root"
-	local -r user="${2:-"$(
-		__get_ssh_user
-	)"}"
-
-	local value="${SSH_USER_HOME}"
-
-	if ! __is_valid_ssh_user_home "${value}"
-	then
-		if [[ ${user} == root ]]
-		then
-			value="${root_value}"
-		else
-			value="${default_value}"
-		fi
-	fi
-
-	# Replace %u with SSH_USER
-	value="${value//'%u'/${user}}"
-
-	printf -- '%s' "${value}"
-}
-
 function __get_ssh_user ()
 {
 	local -r default_value="${1:-app-admin}"
@@ -118,24 +63,9 @@ function main ()
 		__get_password "${password_length}"
 	)"}"
 	ssh_user="$(__get_ssh_user)"
-	ssh_user_home="$(
-		__get_ssh_user_home
-	)"
-	if [[ ${ssh_user} == root ]]
+	if [[ ${ssh_user} != root ]]
 	then
-		chsh \
-			-s "${ssh_user_shell}" \
-			"${ssh_user}" \
-			&> /dev/null
-	else
-		# Create base directory for home
-		if [[ -n ${ssh_user_home%/*} ]] \
-			&& [[ ! -d ${ssh_user_home%/*} ]]
-		then
-			mkdir -pm 755 "${ssh_user_home%/*}"
-		fi
 		useradd -m \
-			-d "${ssh_user_home}" \
 			"${ssh_user}"
 		printf -- \
 					'%s:%s\n' \
