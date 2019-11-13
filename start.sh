9f84f63e78d3e2024fc178c22b1d49f458ed7f1e
gasfor <smx341@126.com>
2019-11-13 18:42:31 +0800

    add environment scripts

--- start.sh
+++ start.sh
@@ -88,9 +88,7 @@ function main ()
 			| chpasswd
 	echo -e "root password:${ssh_root_password}"
 	echo -e "${ssh_user} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/promission
-	echo 'sudo su -' >> /etc/profile
-	echo 'export $(cat /proc/1/environ |tr '\''\0'\'' '\''\n'\'' | xargs)' >> /etc/profile
-	echo 'logout' >> /etc/profile
+	echo 'sudo su -c '"'./environment.sh'"'' >> /etc/profile
 }
 
 main "${@}"
